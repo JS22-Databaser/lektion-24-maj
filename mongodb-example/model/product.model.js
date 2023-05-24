b58810747d74a63ef0120b5d90d629e00c43b146
zocom-christoffer-wallenberg <54267140+zocom-christoffer-wallenberg@users.noreply.github.com>
2023-05-24 13:47:51 +0200

    Added code

---
+++ mongodb-example/model/product.model.js
@@ -0,0 +1,15 @@
+const Products = require('./product.schema');
+
+async function getProductById(id) {
+    // SELECT * FROM Products WHERE id = id
+
+    const product = await Products.findOne({ id });
+    return product;
+}
+
+async function getProducts() {
+    const allProducts = await Products.find();
+    return allProducts;
+}
+
+module.exports = { getProducts, getProductById }
